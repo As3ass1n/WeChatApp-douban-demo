39259fe7cfcb08f98733d2f6edefae627afe6ebe
wpx <811273998@qq.com>
2016-10-01 14:36:06 +0800

    第二步，上拉加载更多完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,34 +3,39 @@ var app = getApp()
 var API_URL = "https://api.douban.com/v2/movie/top250"
 Page({
   data: {
-    page:1,
-    size:20,
-    title:"数据正在加载中...",
-    movie:[],
-    loading:true,
+    page: 1,
+    size: 20,
+    title: "数据正在加载中...",
+    movie: [],
+    loading: true,
     hasMore: true
   },
 
-  onLoad: function () {
+  onLoad: function() {
     var _this = this
-    app.fetchApi(API_URL,function (err, data) {
+    app.fetchApi(API_URL, 0, function(err, data) {
       console.log(data);
       _this.setData({
-        title:data.title,
-        movies:data.subjects,
+        title: data.title,
+        movies: data.subjects,
         loading: false
       })
     })
   },
-  lower: function () {
+  loadMore: function() {
     var _this = this
-    if(_this.data.hasMore) return
-    
-    app.fetchApi(API_URL,function (err,data) {
-      
-
-      //TODO
-      // 上拉加载更多  查看angular中下一页请求的数据方式
+    var start = (_this.data.page++)*_this.data.size
+    app.fetchApi(API_URL, start, function(err, data) {
+      console.log(data);
+      if (start) {
+        _this.setData({
+          title: data.title,
+          movies: data.subjects,
+          loading: false
+        })
+      }else{
+        console.log("电影已加载完毕。。。");
+      }
     })
   }
 })
