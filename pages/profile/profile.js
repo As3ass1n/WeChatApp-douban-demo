b15f3a8f8241841c9436d3e3ee6ac30623085497
wpx <811273998@qq.com>
2016-10-02 21:17:43 +0800

    初版完成

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,1 +1,20 @@
 'use strict';
+var app = getApp()
+Page({
+	data:{
+		userName:"",
+		userAvatar:""
+	},
+	onLoad:function () {
+		var _this = this
+		wx.getUserInfo({
+			success:function (res) {
+				console.log(res.userInfo);
+				_this.setData({
+					userName:res.userInfo.nickName,
+					userAvatar:res.userInfo.avatarUrl
+				})
+			}
+		})
+	}
+})
